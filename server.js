18fdda53f6705b6d146cc8acfd939f62480a2dbf
Shekhar <guptashekhar575@gmail.com>
2024-04-10 01:40:37 +0530

    server file changes

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.json({ extended: false }))
 // Server static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static(path.join(__dirname, 'client', 'build', 'index.html')))
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 }
 
 // Define routes
