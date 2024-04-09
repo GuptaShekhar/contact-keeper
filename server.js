0047cbb068e9fce66ec31fadf823c37bb2377bfe
Shekhar <guptashekhar575@gmail.com>
2024-04-10 02:17:22 +0530

    server file changes

--- server.js
+++ server.js
@@ -9,19 +9,20 @@ connectDB()
 
 // Init Middleware
 app.use(express.json({ extended: false }))
-// Set static folder
-app.use(express.static('client/build'));
-
-// Server static assets in production
-if (process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    );
-}
 
 // Define routes
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
